e876f7382487b2ab6c225ea9b846bcb83fbf3aa0
Zaira María Celis Rueda <zcelis513@unab.edu.co>
2025-10-23 11:47:28 -0500

    Setup navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -56,4 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
 }
--- app/src/main/java/me/zairacelis/unabshop/MainActivity.kt
+++ app/src/main/java/me/zairacelis/unabshop/MainActivity.kt
@@ -12,37 +12,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import me.zairacelis.unabshop.ui.theme.UnabShopTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UnabShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            val startDestination = "Login"
+            NavHost(
+                navController = navController,
+                startDestination = startDestination,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                composable(route = "Login") {
+                    LoginScreen()
+                }
+                composable(route = "register") {
+                    RegisterScreen()
+                }
+                composable(route = "home") {
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnabShopTheme {
-        Greeting("Android")
     }
 }
